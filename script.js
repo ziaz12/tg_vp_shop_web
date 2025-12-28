7caa73aafdbfb8d40486312d7fc489c033526aad
ziaz12 <132633194+ziaz12@users.noreply.github.com>
2025-12-27 23:00:45 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -170,19 +170,20 @@ function updateCartUI() {
     });
 
     // ===== КНОПКА СКРОЛЛ ВВЕРХ =====
-    const scrollBtn = document.getElementById("scrollTopBtn");
+const scrollBtn = document.getElementById("scrollTopBtn");
 
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 200) {
-            scrollBtn.style.display = "block";
-        } else {
-            scrollBtn.style.display = "none";
-        }
-    });
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+});
 
-    scrollBtn.addEventListener("click", () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    });
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
 
     // ===== КНОПКА ПЕРЕХОДА В КОРЗИНУ =====
     cartButton.addEventListener("click", () => {
